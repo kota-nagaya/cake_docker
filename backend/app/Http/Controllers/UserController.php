56c52feee071aa638a9df6e14df2631797671e6b
kota623 <kota.sozosha@gmail.com>
2021-02-16 21:56:57 +0900

    first commit

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+      $this->middleware('auth');
+  }
+
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Contracts\Support\Renderable
+   */
+  public function index()
+  {
+    $users = User::all();
+    var_dump($users[0]->name);
+    // var_dump($users[0]["name"]);
+
+    return view('user', ['users' => $users]);
+  }
+}
